b6fefd20d5d13adb6aa2e6c42a0c3a81339960a0
stdenisj <stdenisjames@gmail.com>
2020-02-04 20:08:49 -0500

    updated js to add classes after creating divs

--- script.js
+++ script.js
@@ -1,10 +1,24 @@
 function createGameBoard() {
  let tile = document.createElement('div');
+ let board = document.querySelector('#Gameboard');
  let i = 0;
  while (i < 30) {
     let tile = document.createElement('div');
-    document.querySelector('#Gameboard').appendChild(tile);
-    document.querySelector('#Gameboard').lastChild.textContent = 'hello world';
+    board.appendChild(tile);
+    board.lastChild.textContent = 'hello world';
+    if (i < 5) {
+        board.lastChild.classList.add('catagory');
+    } else if ((i < 10) && (i > 4)) {
+            board.lastChild.classList.add('onehundred');
+    } else if ((i < 15) && (i > 9)) {
+            board.lastChild.classList.add('twohundred');
+    } else if ((i < 20) && (i > 14)) {
+            board.lastChild.classList.add('threehundred');
+    } else if ((i < 25) && (i > 19)) {
+            board.lastChild.classList.add('fourhundred');
+    } else if((i < 30) && (i > 24)){
+            board.lastChild.classList.add('fivehundred');
+    }
     i++;
  }
 }
