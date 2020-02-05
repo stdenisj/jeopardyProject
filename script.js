a4b469b2ea72558965d127b82d5bba2fcc0aa8eb
stdenisj <stdenisjames@gmail.com>
2020-02-04 22:34:43 -0500

    updated creatGameBoard to include $

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+
+let divsArr = [];
+
 function createGameBoard() {
  let tile = document.createElement('div');
  let board = document.querySelector('#Gameboard');
@@ -9,20 +12,29 @@ function createGameBoard() {
         board.lastChild.classList.add('catagory');
     } else if ((i < 10) && (i > 4)) {
             board.lastChild.classList.add('onehundred');
-            board.lastChild.textContent = '100';
+            board.lastChild.textContent = '$100';
     } else if ((i < 15) && (i > 9)) {
             board.lastChild.classList.add('twohundred');
-            board.lastChild.textContent = '200';
+            board.lastChild.textContent = '$200';
     } else if ((i < 20) && (i > 14)) {
             board.lastChild.classList.add('threehundred');
-            board.lastChild.textContent = '300';
+            board.lastChild.textContent = '$300';
     } else if ((i < 25) && (i > 19)) {
             board.lastChild.classList.add('fourhundred');
-            board.lastChild.textContent = '400';
+            board.lastChild.textContent = '$400';
     } else if((i < 30) && (i > 24)){
             board.lastChild.classList.add('fivehundred');
-            board.lastChild.textContent = '500';
+            board.lastChild.textContent = '$500';
     }
     i++;
- }
+    }
+divsArr = document.querySelectorAll('div.catagory, div.onehundred, div.twohundred, div.threehundred, div.fourhundred, div.fivehundred')
+}
+
+function activateDivs() {
+    divsArr.forEach(div => {
+        div.addEventListener('click', (event) => {
+            console.log(event);
+        })
+    })
 }
